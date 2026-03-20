c387817c9cef3bd5829cabbc886055c4d30b5d0a
CMVanwagoner <cmvanwagoner@gmail.com>
2026-03-20 16:24:03 -0600

    Original 05 Upload

---
+++ 05/js/reviews.js
@@ -0,0 +1,32 @@
+export const reviews = [
+   {
+      "name":"Cowey Wandall",
+      "stars":"3",
+      "text":"W Nake."
+   },
+   {
+      "name":"Stockton Harding",
+      "stars":"5",
+      "text":"Would recommend 5/5"
+   },
+   {
+      "name":"Trevor Cameraman",
+      "stars":"3",
+      "text":"Not a good place to sell cameras"
+   },
+   {
+      "name":"Pete & Repeat",
+      "stars":"4",
+      "text":"We had an excellent time selling our car and finding the next vehicle for our travels."
+   },
+   {
+      "name":"Jeremy Jickwood",
+      "stars":"4",
+      "text":"The staff treated me with kindness and patience"
+   },
+   {
+      "name":"Kollin Doo",
+      "stars":"5",
+      "text":"I would return"
+   }
+]

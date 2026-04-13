0c7032533795650ad269d162070d8bea031e501c
CMVanwagoner <cmvanwagoner@gmail.com>
2026-04-13 10:26:33 -0600

    pathing fix

--- 010/data/listing.js
+++ 010/data/listing.js
@@ -2,8 +2,8 @@ export const listings =  [
   {
     "name": "Dust Hollow Camp",
     "image": {
-      "small": "../images/tent1small.webp",
-      "large": "../images/tent1large.webp"
+      "small": "..images/tent1small.webp",
+      "large": "..images/tent1large.webp"
     },
     "address": ["1423 Rusted Rail Rd", "Provo, UT 84601"],
     "phone": "801-555-1001"
@@ -11,8 +11,8 @@ export const listings =  [
   {
     "name": "Broken Lantern Village",
     "image": {
-      "small": "../images/tent2small.webp",
-      "large": "../images/tent2large.webp"
+      "small": "..images/tent2small.webp",
+      "large": "..images/tent2large.webp"
     },
     "address": ["587 Ember Street", "Orem, UT 84057"],
     "phone": "801-555-1002"
@@ -20,8 +20,8 @@ export const listings =  [
   {
     "name": "Cinder Patch",
     "image": {
-      "small": "../images/tent3small.webp",
-      "large": "../images/tent3large.webp"
+      "small": "..images/tent3small.webp",
+      "large": "..images/tent3large.webp"
     },
     "address": ["903 Ashwood Ave", "Springville, UT 84663"],
     "phone": "801-555-1003"
@@ -29,8 +29,8 @@ export const listings =  [
   {
     "name": "Tin Roof Colony",
     "image": {
-      "small": "../images/tent4small.webp",
-      "large": "../images/tent4large.webp"
+      "small": "..images/tent4small.webp",
+      "large": "..images/tent4large.webp"
     },
     "address": ["221 Scrapline Drive", "Lehi, UT 84043"],
     "phone": "801-555-1004"
@@ -38,8 +38,8 @@ export const listings =  [
   {
     "name": "Quiet Tarp Camp",
     "image": {
-      "small": "../images/tent5small.webp",
-      "large": "../images/tent5large.webp"
+      "small": "..images/tent5small.webp",
+      "large": "..images/tent5large.webp"
     },
     "address": ["764 Hollow Creek Ln", "Spanish Fork, UT 84660"],
     "phone": "801-555-1005"
@@ -47,8 +47,8 @@ export const listings =  [
   {
     "name": "Iron Stake Settlement",
     "image": {
-      "small": "../images/tent6small.webp",
-      "large": "../images/tent6large.webp"
+      "small": "..images/tent6small.webp",
+      "large": "..images/tent6large.webp"
     },
     "address": ["1189 Gravel Bend Rd", "Payson, UT 84651"],
     "phone": "801-555-1006"
@@ -56,8 +56,8 @@ export const listings =  [
   {
     "name": "Driftwood Corner",
     "image": {
-      "small": "../images/tent7small.webp",
-      "large": "../images/tent7large.webp"
+      "small": "..images/tent7small.webp",
+      "large": "..images/tent7large.webp"
     },
     "address": ["452 Windbreak Blvd", "American Fork, UT 84003"],
     "phone": "801-555-1007"
@@ -65,8 +65,8 @@ export const listings =  [
   {
     "name": "Canvas Ridge Camp",
     "image": {
-      "small": "../images/tent8small.webp",
-      "large": "../images/tent8large.webp"
+      "small": "..images/tent8small.webp",
+      "large": "..images/tent8large.webp"
     },
     "address": ["3001 Ridgeback Trail", "Pleasant Grove, UT 84062"],
     "phone": "801-555-1008"
